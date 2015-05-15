9b372f05ae69013d4293b3873f96753af59b573e
David Shumway <davidshumway@gmail.com>
2015-05-15 01:29:46 -0400

    Add 2nd try. Needs ignore autofocus.

--- OSM iD Browser Tool 2/script.js
+++ OSM iD Browser Tool 2/script.js
@@ -26,265 +26,10 @@ AllPages.prototype.helper__location = function() {
  * -
  * -
  */
-AllPages.prototype.func_preset = function(context) {
-	//~ iD.ui.PresetList = function(context) {
-	//~ var context = id;
-	
-	var event = d3.dispatch('choose'),
-		id,
-		currentPreset,
-		autofocus = false;
-
-	function presetList(selection) {
-		var geometry = context.geometry(id),
-			presets = context.presets().matchGeometry(geometry);
-
-		selection.html('');
-
-		var messagewrap = selection.append('div')
-			.attr('class', 'header fillL cf');
-
-		var message = messagewrap.append('h3')
-			.text(t('inspector.choose'));
-
-		if (context.entity(id).isUsed(context.graph())) {
-			messagewrap.append('button')
-				.attr('class', 'preset-choose')
-				.on('click', function() { event.choose(currentPreset); })
-				.append('span')
-				.attr('class', 'icon forward');
-		} else {
-			messagewrap.append('button')
-				.attr('class', 'close')
-				.on('click', function() {
-					context.enter(iD.modes.Browse(context));
-				})
-				.append('span')
-				.attr('class', 'icon close');
-		}
-
-		function keydown() {
-			// DEBUG
-			console.log('=========');
-			console.log(d3.event.keyCode);
-			console.log(d3.event);
-			//~ "} else if (search.property('value').length === 0 &&"+
-					//~ "(d3.event.ctrlKey || d3.event.metaKey) &&"+
-					//~ "d3.event.keyCode === d3.keybinding.keyCodes['"+char+"']) {"+
-					//~ "d3.event.preventDefault();"+
-					//~ "d3.event.stopPropagation();console.log(d3.event);";
-			// hack to let delete shortcut work when search is autofocused
-			if (search.property('value').length === 0 &&
-				(d3.event.keyCode === d3.keybinding.keyCodes['⌫'] ||
-				 d3.event.keyCode === d3.keybinding.keyCodes['⌦'])) {
-				d3.event.preventDefault();
-				d3.event.stopPropagation();
-				iD.operations.Delete([id], context)();
-			} else if (search.property('value').length === 0 &&
-				(d3.event.ctrlKey || d3.event.metaKey) &&
-				d3.event.keyCode === d3.keybinding.keyCodes.z) {
-				d3.event.preventDefault();
-				d3.event.stopPropagation();
-				context.undo();
-			} else if (!d3.event.ctrlKey && !d3.event.metaKey) {
-				d3.select(this).on('keydown', null);
-			}
-		}
-
-		function keypress() {
-			// enter
-			var value = search.property('value');
-			if (d3.event.keyCode === 13 && value.length) {
-				list.selectAll('.preset-list-item:first-child').datum().choose();
-			}
-		}
-
-		function inputevent() {
-			var value = search.property('value');
-			list.classed('filtered', value.length);
-			if (value.length) {
-				var results = presets.search(value, geometry);
-				message.text(t('inspector.results', {
-					n: results.collection.length,
-					search: value
-				}));
-				list.call(drawList, results);
-			} else {
-				list.call(drawList, context.presets().defaults(geometry, 36));
-				message.text(t('inspector.choose'));
-			}
-		}
-
-		var searchWrap = selection.append('div')
-			.attr('class', 'search-header');
-
-		var search = searchWrap.append('input')
-			.attr('class', 'preset-search-input')
-			.attr('placeholder', t('inspector.search'))
-			.attr('type', 'search')
-			.on('keydown', keydown)
-			.on('keypress', keypress)
-			.on('input', inputevent);
-
-		searchWrap.append('span')
-			.attr('class', 'icon search');
-
-		if (autofocus) {
-			search.node().focus();
-		}
-
-		var listWrap = selection.append('div')
-			.attr('class', 'inspector-body');
-
-		var list = listWrap.append('div')
-			.attr('class', 'preset-list fillL cf')
-			.call(drawList, context.presets().defaults(geometry, 36));
-	}
-
-	function drawList(list, presets) {
-		var collection = presets.collection.map(function(preset) {
-			return preset.members ? CategoryItem(preset) : PresetItem(preset);
-		});
-
-		var items = list.selectAll('.preset-list-item')
-			.data(collection, function(d) { return d.preset.id; });
-
-		items.enter().append('div')
-			.attr('class', function(item) { return 'preset-list-item preset-' + item.preset.id.replace('/', '-'); })
-			.classed('current', function(item) { return item.preset === currentPreset; })
-			.each(function(item) {
-				d3.select(this).call(item);
-			})
-			.style('opacity', 0)
-			.transition()
-			.style('opacity', 1);
-
-		items.order();
-
-		items.exit()
-			.remove();
-	}
-
-	function CategoryItem(preset) {
-		var box, sublist, shown = false;
-
-		function item(selection) {
-			var wrap = selection.append('div')
-				.attr('class', 'preset-list-button-wrap category col12');
-
-			wrap.append('button')
-				.attr('class', 'preset-list-button')
-				.call(iD.ui.PresetIcon()
-					.geometry(context.geometry(id))
-					.preset(preset))
-				.on('click', item.choose)
-				.append('div')
-				.attr('class', 'label')
-				.text(preset.name());
-
-			box = selection.append('div')
-				.attr('class', 'subgrid col12')
-				.style('max-height', '0px')
-				.style('opacity', 0);
-
-			box.append('div')
-				.attr('class', 'arrow');
-
-			sublist = box.append('div')
-				.attr('class', 'preset-list fillL3 cf fl');
-		}
-
-		item.choose = function() {
-			if (shown) {
-				shown = false;
-				box.transition()
-					.duration(200)
-					.style('opacity', '0')
-					.style('max-height', '0px')
-					.style('padding-bottom', '0px');
-			} else {
-				shown = true;
-				sublist.call(drawList, preset.members);
-				box.transition()
-					.duration(200)
-					.style('opacity', '1')
-					.style('max-height', 200 + preset.members.collection.length * 80 + 'px')
-					.style('padding-bottom', '20px');
-			}
-		};
-
-		item.preset = preset;
-
-		return item;
-	}
-
-	function PresetItem(preset) {
-		function item(selection) {
-			var wrap = selection.append('div')
-				.attr('class', 'preset-list-button-wrap col12');
-
-			wrap.append('button')
-				.attr('class', 'preset-list-button')
-				.call(iD.ui.PresetIcon()
-					.geometry(context.geometry(id))
-					.preset(preset))
-				.on('click', item.choose)
-				.append('div')
-				.attr('class', 'label')
-				.text(preset.name());
-
-			wrap.call(item.reference.button);
-			selection.call(item.reference.body);
-		}
-
-		item.choose = function() {
-			context.presets().choose(preset);
-
-			context.perform(
-				iD.actions.ChangePreset(id, currentPreset, preset),
-				t('operations.change_tags.annotation'));
-
-			event.choose(preset);
-		};
-
-		item.help = function() {
-			d3.event.stopPropagation();
-			item.reference.toggle();
-		};
-
-		item.preset = preset;
-		item.reference = iD.ui.TagReference(preset.reference(context.geometry(id)), context);
-
-		return item;
-	}
-
-	presetList.autofocus = function(_) {
-		if (!arguments.length) return autofocus;
-		return false;
-		
-		autofocus = _;
-		return presetList;
-	};
-
-	presetList.entityID = function(_) {
-		if (!arguments.length) return id;
-		id = _;
-		presetList.preset(context.presets().match(context.entity(id), context.graph()));
-		return presetList;
-	};
-
-	presetList.preset = function(_) {
-		if (!arguments.length) return currentPreset;
-		currentPreset = _;
-		return presetList;
-	};
-
-	return d3.rebind(presetList, event, 'on');
-}
 AllPages.prototype.openstreetmap = function() {
 	
 	// global scope vars
-	var sb, d1, d2; // localStorage vars
+	var sb // localStorage vars
 	var interval_images; // Interval for CSS images
 	var o = {
 		sb: [ // Sidebar search field. It is necessary to move this when "opening" tools menu.
@@ -295,14 +40,12 @@ AllPages.prototype.openstreetmap = function() {
 		},
 		hotkeys: {
 		},
-		hk_notes: 'NOTE: For hotkeys hold CTRL plus hotkey to activate (⌘ plus hotkey on Mac). An area must be selected for the hotkey to correctly work. To change an existing area using the hotkeys, the area must first be selected before running the hotkey.\nTest new line.'
-		//~ exec_choices: [ //0...4
-			//~ 'Add New Point',
-			//~ 'Add New Line',
-			//~ 'Add New Area',
-			//~ 'Do Nothing',
-		//~ ]
-		//~ initial_preset
+		hk_notes: 'NOTE: For hotkeys hold CTRL plus hotkey to activate (⌘ plus hotkey on Mac). An area must be selected for the hotkey to correctly work. To change an existing area using the hotkeys, the area must first be selected before running the hotkey.\nTest new line.',
+		dv: {
+			d1: null,
+			d2: null
+		},
+		sidebar: document.getElementById('sidebar')
 	}
 	
 	// init_ls_vars
@@ -392,20 +135,17 @@ AllPages.prototype.openstreetmap = function() {
 		// Init localStorage vars
 		init_ls_vars();
 		
-		// Sidebar
-		var sb = document.getElementById('sidebar');
-		
 		// Div container
-		var d1 = document.createElement('div');
-		d1.setAttribute('style','border-bottom:1px solid #a2a2a2;');
-		sb.insertBefore(d1, sb.firstChild); // Append
+		o.dv.d1 = document.createElement('div');
+		o.dv.d1.setAttribute('style','border-bottom:1px solid #a2a2a2;');
+		sidebar.insertBefore(o.dv.d1, sidebar.firstChild); // Append
 		// Open / close button
-		var d2 = document.createElement('div'); // "button" causes bad CSS.
-		d2.setAttribute('style','width:100%;height:20px;text-align:center;cursor:pointer;font-family:courier;border:1px solid #a2a2a2;background-color:white;');
-		d2.innerText = 'TOOLS'; //(a;b;c;)
-		d2.is_open = false;
-		d1.appendChild(d2);
-		d2.addEventListener('click', function(e) {
+		o.dv.d2 = document.createElement('div'); // "button" causes bad CSS.
+		o.dv.d2.setAttribute('style','width:100%;height:20px;text-align:center;cursor:pointer;font-family:courier;border:1px solid #a2a2a2;background-color:white;');
+		o.dv.d2.innerText = 'TOOLS'; //(a;b;c;)
+		o.dv.d2.is_open = false;
+		o.dv.d1.appendChild(o.dv.d2);
+		o.dv.d2.addEventListener('click', function(e) {
 			console.log(this.is_open);
 			if (!this.is_open) {
 				this.style.border = '2px solid #a2a2a2;';
@@ -838,165 +578,23 @@ AllPages.prototype.openstreetmap = function() {
 	}
 	// Add preset + values
 	// Pass array
-	function p_init_preset() {//p_init_hotkeys();return;
-		var ar = [];
-		for (var hk in o.hotkeys) {
-			ar.push(hk);
-		}
-		var ap = JSON.stringify({'hk':ar, 'preset': new AllPages().func_preset + ''});
-		js_append(page__preset, ap); // new // '' + 
+	function p_init_preset() {
+		
 	}
 	// Add hotkeys
 	// 
 	function p_init_hotkeys() {
 		js_append(page__hotkeys, JSON.stringify(o.hotkeys));
-		//~ js_append(page__hotkeys, {hotkeys:o.hotkeys, preset: '' + preset_list_str});
 	}
 	
 	// Preset
-	// old
-	// Does not work. See below.
 	function page__preset(hotkeys) {
 		
-		//~ var func = '' + iD.ui.PresetList;
-		
-		var func = hotkeys.preset;
-		var hotkeys = hotkeys.hk;
-		//~ 
-		//~ console.log(func);
-		//~ console.log(hotkeys);return;
-		
-		if (func.indexOf('} else if (search.property(\'value\').length === 0 &&') == -1) {
-			alert('Warning: OSM iD Browser Tool: Cannot find PresetList!');
-			return;
-		}
-		
-		//~ // Make a backup copy. This is so that it is possible to continously change hotkeys and not have to work with altered value.
-		//~ if (!iD.ui.hasOwnProperty('PresetList__backup_copy_id_browser_tool')) {
-			//~ iD.ui.PresetList__backup_copy_id_browser_tool = iD.ui.PresetList;
-		//~ }
-		
-		// add_to_preset
-		var add_to_preset = '';
-		for (var i in hotkeys) {
-			var char = hotkeys[ i ];
-			if (char.length != 1) continue;
-			add_to_preset +=
-				//~ '0===v.property("value").length&&(d3.event.ctrlKey||d3.event.metaKey)&&d3.event.keyCode===d3.keybinding.keyCodes["'+char+'"]?('+
-				//~ 'd3.event.preventDefault();'+
-				//~ 'd3.event.stopPropagation(); ) :';
-								
-				"} else if (search.property('value').length === 0 &&"+
-					"(d3.event.ctrlKey || d3.event.metaKey) &&"+
-					"d3.event.keyCode === d3.keybinding.keyCodes['"+char+"']) {"+
-					"d3.event.preventDefault();"+
-					"d3.event.stopPropagation();console.log(d3.event);";
-		}
-		console.log('ADD:'+add_to_preset);
-		if (add_to_preset == '') return;
-		
-		// func
-		// Replace with new Function('context', functionToText)
-		//~ var func = '' + iD.ui.PresetList__backup_copy_id_browser_tool;
-		// Replace with blank: function(context) {}
-		//~ func = func.replace(/^ *function *\(e\) *\{/, '');
-		
-		func = func.replace(/^ *function *\(context\) *\{/, '');
-		func = func.replace(/ *\} *$/, '');
-		func =
-			func
-			.replace(
-				//~ '?0===v.property("value").length&&(d3.event.ctrlKey||d3.event.metaKey)',
-				//~ '?' +
-				//~ add_to_preset +
-				//~ '0===v.property("value").length&&(d3.event.ctrlKey||d3.event.metaKey)'
-
-				'} else if (search.property(\'value\').length === 0 &&',
-				add_to_preset +
-				'} else if (search.property(\'value\').length === 0 &&'
-			);
-		//~ console.log(func);
-		iD.ui.PresetList = new Function('context', func);
-		iD.ui.PresetList(id);
-		//~ console.log(iD.ui.PresetList);
-		
-		
-		//~ console.log('x');
-		//~ console.log(iD.ui.PresetList);
 	}
-	/*TRY 1// Preset
-	// old
-	// Does not work. See below.
-	function page__preset(hotkeys) {
-		
-		//?0===v.property("value").length&&(d3.event.ctrlKey||d3.event.metaKey)&&d3.event.keyCode===d3.keybinding.keyCodes.z
-		
-		var func = '' + iD.ui.PresetList;
-		console.log(func);
-		console.log(hotkeys);
-		
-		
-		//~ if (func.indexOf('} else if (search.property(\'value\').length === 0 &&') == -1) {
-		if (func.indexOf('?0===v.property("value").length&&(d3.event.ctrlKey||d3.event.metaKey)&&d3.event.keyCode===d3.keybinding.keyCodes.z') == -1) {
-			alert('Warning: OSM iD Browser Tool: Cannot find PresetList!');
-			return;
-		}
-		
-		// Make a backup copy. This is so that it is possible to continously change hotkeys and not have to work with altered value.
-		if (!iD.ui.hasOwnProperty('PresetList__backup_copy_id_browser_tool')) {
-			iD.ui.PresetList__backup_copy_id_browser_tool = iD.ui.PresetList;
-		}
-		// add_to_preset
-		var add_to_preset = '';
-		for (var i in hotkeys) {
-			var char = hotkeys[ i ];
-			if (char.length != 1) continue;
-			add_to_preset +=
-				'0===v.property("value").length&&(d3.event.ctrlKey||d3.event.metaKey)&&d3.event.keyCode===d3.keybinding.keyCodes["'+char+'"]?('+
-				'd3.event.preventDefault();'+
-				'd3.event.stopPropagation(); ) :';
-								
-				//~ "} else if (search.property('value').length === 0 &&"+
-					//~ "(d3.event.ctrlKey || d3.event.metaKey) &&"+
-					//~ "d3.event.keyCode === d3.keybinding.keyCodes['"+char+"']) {"+
-					//~ "d3.event.preventDefault();"+
-					//~ "d3.event.stopPropagation();";
-		}
-		console.log('ADD:'+add_to_preset);
-		if (add_to_preset == '') return;console.log('x');
-		// func
-		// Replace with new Function('context', functionToText)
-		var func = '' + iD.ui.PresetList__backup_copy_id_browser_tool;
-		// Replace with blank: function(context) {}
-		func = func.replace(/^ *function *\(e\) *\{/, '');
-		func = func.replace(/ *\} *$/, '');
-		func =
-			func
-			.replace(
-				'?0===v.property("value").length&&(d3.event.ctrlKey||d3.event.metaKey)',
-				'?' +
-				add_to_preset +
-				'0===v.property("value").length&&(d3.event.ctrlKey||d3.event.metaKey)'
-
-				//~ '} else if (search.property(\'value\').length === 0 &&',
-				//~ add_to_preset +
-				//~ '} else if (search.property(\'value\').length === 0 &&'
-			);
-		console.log(func);
-		//~ console.log(iD.ui.PresetList);
-		iD.ui.PresetList = new Function('e', func);
-		console.log(iD.ui.PresetList);
-	}*/
 	// e.g. new iD.actions.ChangeTags('w-1', {'building':'yes'})(id.graph()) ;
-	function page__hotkeys(hotkeys_obj) { //, hotkeys_obj_stringified) {
+	function page__hotkeys(hotkeys_obj) {
 		
 		var context = id;
-		//~ alert(hotkeys_obj);return;
-		//~ try {
-			//~ var hotkeys = JSON.parse(hotkeys_obj_stringified);
-		//~ } catch(e) {
-			//~ return;
-		//~ }
 		
 		function bToolChangeTags_valid(e) {
 			for (var i in e) {
@@ -1055,7 +653,6 @@ AllPages.prototype.openstreetmap = function() {
 		
 		// e.g. 'm':{ char: 'm', enabled: true, tags: 'buildings=yes', exec_next: true, square: true }
 		function bToolChangeTags() {
-			//~ var context = id;
 			// console.log(this); // e.g. Object {event: Object, capture: undefined, callback: function}
 			
 			if (!this.event.ctrlKey) return; // Must be CTRL.
